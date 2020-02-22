2d708802cb8e424e30def0d0fabf5db7ad5c65e4
Tetewet <theogorisse@gmail.com>
2020-02-22 12:20:28 -0500

    added some skills

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour
+{
+    //string skillName;
+    //string skillDescription;
+    //private int skillPoint;
+    //private const int skillPointMax = 2;
+
+    public Skill(string skillName, string skillDescription, int skillPoint)
+    {
+        //this.skillName = skillName;
+        //this.skillDescription = skillDescription;
+        //this.skillPoint = skillPoint;
+    }
+
+    public virtual void Activate()
+    {
+        //skillPoint--;
+    }
+
+    public virtual void Replenish()
+    {
+        //if (skillPoint < skillPointMax)
+        //{
+        //    skillPoint++;
+        //}
+    }
+
+    //public virtual string[] ReadSkill()
+    //{
+    //    //string[] result = new string[2];
+    //    //result[0] = skillName;
+    //    //result[1] = skillDescription;
+    //    //return result;
+    //}
+}
