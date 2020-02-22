b866d34b2946f5f259e9575762b251950fe4c335
Philip Frattolillo <phil.fratt@gmail.com>
2020-02-22 15:59:08 -0500

    Events
    Added support for events

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -6,21 +6,34 @@ public class Events : MonoBehaviour
 {
     void ArtilleryStrike()
     {
+        //get random position
+
+        //detect mines around the position
+
+        //destroy mines that were detected
+
+        //injure soldiers and nurses in the area
 
     }
 
     void HailOfBullets()
     {
+        //get random position
 
+        //select the column
+
+        //get range (row)
+
+        //injure soldiers and nurses in the area (first to be hit?)
     }
 
     void SpawnMine()
     {
-
+        //spawn a mine in a random unoccupied space
     }
 
     void SpawnSoldier()
     {
-
+        //spawn a soldier in a random unoccupied space
     }
 }
