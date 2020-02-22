ace7da28d8e056dee1e57840fb408f79da549f5f
Ella <ellazhang37@gmail.com>
2020-02-22 17:36:36 -0500

    + Added Logic in Nurse Scene

--- Assets/Ella/NurseScene.cs
+++ Assets/Ella/NurseScene.cs
@@ -1,20 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NurseScene : MonoBehaviour
 {
     Nurse currentNurse;
-    [SerializeField] List<Nurse> nurseList;
+    Text positionTxt;
+    //Position position;
+    [SerializeField] Image nurseImage;
+    Sprite nursePoetry;
+    [SerializeField] int nIndex;
+
+    [Space]
+
+    [SerializeField] Sprite nurseImage1;
+    [SerializeField] Sprite nurseImage2;
+    [SerializeField] Sprite nurseImage3;
+    [SerializeField] Sprite nurseImage4;
+
+
+    UIMessager uiMessager;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        positionTxt = GameObject.Find("LocationTxt").GetComponent<Text>();
+        //position = GetComponent<Position>();
+        uiMessager = GetComponent<UIMessager>();
+        positionTxt.text = "A1";
+        //nursePoetry = GameObject.Find("NursePoetry").GetComponent<Sprite>();
+        //nurseImage = GameObject.Find("NursePoetry").GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //positionTxt.text = position.Convert();
+        //nursePoetry = ChangePoetry();
+        nurseImage.sprite = SetCurrentIndex(nIndex);
+        positionTxt.text = "A1";
+        Debug.Log("Hello");
+        Debug.Log(positionTxt.name);
+    }
+
+    public void SetIndex(int index)
+    {
+        nIndex = index;
+        Debug.Log(index);
+    }
+
+    Sprite SetCurrentIndex(int index)
+    {
+        Sprite image;
+        switch (index)
+        {
+            case 0:
+                image = nurseImage1;
+                break;
+            case 1:
+                image = nurseImage2;
+                break;
+            case 2:
+                image = nurseImage3;
+                break;
+            default:
+                image = nurseImage4;
+                break;
+
+        }
+        return image;
+    }
+
+    Sprite ChangePoetry()
+    {
+
+        Sprite image;
+        switch (GameLoop.instance.currentNurseToMove)
+        {
+            case 0:
+                image = nurseImage1;
+                break;
+            case 1:
+                image = nurseImage2;
+                break;
+            case 2:
+                image = nurseImage3;
+                break;
+            default:
+                image = nurseImage4;
+                break;
+
+        }
+        return image;
     }
 }
