9daeb674e3ac76287af7bd80dc9f4e60967273cd
Xun Ruan <galaxymaxx@outlook.com>
2020-02-23 16:06:58 -0500

    Adjust Debug Panel

--- Assets/ContextualTextPanel.cs
+++ Assets/ContextualTextPanel.cs
@@ -7,38 +7,26 @@ using UnityEngine.UI;
 
 public class ContextualTextPanel : MonoBehaviour
 {
-    [SerializeField] float lifetime = 4f;
-    private float lifeTimeCounter;
     [SerializeField] Text message;
+    [SerializeField] private int maxInfoToDisplay = 4;
 
     public Queue<string> messagesToShow;
     private StringBuilder sb;
     
     private void OnEnable()
     {
-        lifeTimeCounter = lifetime;
         sb = new StringBuilder();
         messagesToShow = new Queue<string>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        lifeTimeCounter -= Time.deltaTime;
-        if (lifeTimeCounter <= 0)
-        {
-            gameObject.SetActive(false);
-        }
-    }
+    
 
     public void Print(string s)
     {
         messagesToShow.Enqueue(s);
-        if (messagesToShow.Count > 3)
+        if (messagesToShow.Count > maxInfoToDisplay)
         {
             messagesToShow.Dequeue();
         }
-        lifeTimeCounter = lifetime;
         sb.Clear();
         foreach (var messageToShow in messagesToShow)
         {
