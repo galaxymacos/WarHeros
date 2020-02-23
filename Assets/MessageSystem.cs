450cb3af6f5aa98986dd069910e99af576882d34
Philip Frattolillo <phil.fratt@gmail.com>
2020-02-23 10:52:11 -0500

    Message System
    Message system WIP

---
+++ Assets/MessageSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSystem : MonoBehaviour
+{
+    [SerializeField] ContextualTextPanel messagePanel;
+
+    public static MessageSystem instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void Print(string s)
+    {
+        messagePanel.gameObject.SetActive(true);
+        messagePanel.Print(s);
+    }
+}
