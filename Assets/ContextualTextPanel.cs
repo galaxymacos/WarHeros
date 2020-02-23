d304484300e9711844e29681c0f2d029f70396e8
Philip Frattolillo <phil.fratt@gmail.com>
2020-02-23 10:27:30 -0500

    Player Display Panel
    Added panel to show the player information

---
+++ Assets/ContextualTextPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContextualTextPanel : MonoBehaviour
+{
+    [SerializeField] float lifetime;
+    [SerializeField] Text message;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void Print(string s)
+    {
+        message.text = s;
+    }
+}
